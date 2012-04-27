8892a50e492664e5bec47c3e86cd37ac4906a032
Marcus Carlsson <mc@beepsend.se>
2012-04-27 16:10:12 +0200

    Completions.

--- .zsh/04-completion.zsh
+++ .zsh/04-completion.zsh
@@ -1,1 +1,6 @@
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*:complete:*' cache-path $ZSH_HOME/cache/
+zstyle ':completion:*:complete:*' use-cache true
+
+zstyle ':completion:*' list-dirs-first true
+zstyle ':completion:*:*:users' ignored-patterns avahi bin daemon dbus ftp mail nobody
