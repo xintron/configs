a39232e87d84eb43bed6a2ad01d0078be6c81743
Marcus Carlsson <carlsson.marcus@gmail.com>
2012-05-19 00:19:00 +0200

    More verbose hashed directories and a few new.

--- .zsh/02-hash.zsh
+++ .zsh/02-hash.zsh
@@ -1,3 +1,6 @@
-hash -d h=/usr/share/nginx/http
-hash -d b=$HOME/bin
-hash -d t=/tmp
+hash -d http=/usr/share/nginx/http
+hash -d bin=$HOME/bin
+if [ -d /mnt/files ]; then
+    hash -d videos=/mnt/files/videos
+    hash -d music=/mnt/files/music
+fi
