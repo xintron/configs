111113983b9660b2c1bc8ee9d81af93d1f9e4bc7
Marcus Carlsson <mc@beepsend.se>
2012-09-11 15:06:11 +0200

    zsh: Completion-fixes.

--- .zsh/04-completion.zsh
+++ .zsh/04-completion.zsh
@@ -1,7 +1,12 @@
-zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*:complete:*' cache-path $ZSH_HOME/cache/
 zstyle ':completion:*:complete:*' use-cache true
 
-zstyle ':completion:*' list-dirs-first true
 zstyle ':completion:*' menu select
 zstyle ':completion:*:*:users' ignored-patterns avahi bin daemon dbus ftp mail nobody
+
+# Redirects
+zstyle ':completion:*:*:-redirect-,2>,*:*' file-patterns '*.(log|txt)' '%p:all_files'
+zstyle ':completion:*:*:-redirect-,2>>,*:*' file-patterns '*.(log|txt)' '%p:all_files'
+zstyle ':completion:*:*:-redirect-,>,*:*' file-patterns '*.(log|txt)' '%p:all_files'
+zstyle ':completion:*:*:-redirect-,>>,*:*' file-patterns '*.(log|txt)' '%p:all_files'
