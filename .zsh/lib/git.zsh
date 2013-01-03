3382c46bfbc81e8f6ccf05d36114a21f06280a71
Marcus Carlsson <mc@beepsend.se>
2013-01-03 09:14:14 +0100

    zsh: Git fixes.

--- .zsh/lib/git.zsh
+++ .zsh/lib/git.zsh
@@ -1,8 +1,13 @@
 function git_prompt_info() {
     ref=`git symbolic-ref -q --short HEAD 2>/dev/null` || return
-    echo "$ZSH_GIT_PROMPT_PREFIX$ref$(git_parse_dirty)$ZSH_GIT_PROMPT_POSTFIX"
+    echo "$ZSH_GIT_PROMPT_PREFIX$ref$(git_parse_dirty)$(git_sha)$ZSH_GIT_PROMPT_POSTFIX"
 }
 
 function git_parse_dirty() {
     [[ -n "`git status -s -uno 2>/dev/null`" ]] && echo "$ZSH_GIT_PROMPT_DIRTY"
+}
+
+function git_sha() {
+    sha=`git rev-parse --short HEAD 2>/dev/null` || return
+    echo "$ZSH_GIT_SHA_PREFIX$sha$ZSH_GIT_SHA_POSTFIX"
 }
