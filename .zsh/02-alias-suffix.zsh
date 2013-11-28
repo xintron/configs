1b3fb9ee571ba1bec35d4313165293acc4a32340
Marcus Carlsson <mc@beepsend.se>
2013-11-28 10:48:06 +0100

    Added cabal bin path to PATH and removed vim suffix (zsh alias).

--- .zsh/02-alias-suffix.zsh
+++ .zsh/02-alias-suffix.zsh
@@ -1,9 +1,7 @@
-local -a VIM MPLAYER FEH
+local -a MPLAYER FEH
 
-VIM=(bash c cfg conf cpp css csv diff git gitignore go h hs htm html info ini java js json lisp log lua markdown md mkd nfo pacnew patch php pid PKGBUILD pl py rb sed sh tex theme txt vim xml zsh)
 MPLAYER=(avi flac flv mkv mov mp3 mp4 mpeg mov mpg wmv)
 FEH=(bmp gif ico jpeg jpg png svg)
 
-alias -s $^VIM='vim --'
 alias -s $^MPLAYER='mplayer --'
 alias -s $^FEH='feh --'
