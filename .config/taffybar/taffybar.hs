1af402f73ea62e1e3ad0702fa6e9024dc2fe73ef
Marcus Carlsson <carlsson.marcus@gmail.com>
2015-09-02 15:33:49 +0200

    Text based taffybar - why graphs?

--- .config/taffybar/taffybar.hs
+++ .config/taffybar/taffybar.hs
@@ -1,7 +1,9 @@
 import Data.Char (toLower)
 import Data.List.Split (chunksOf)
 import Numeric (readHex)
+import Text.Printf (printf)
 
+import qualified Graphics.UI.Gtk as Gtk
 import System.Taffybar
 import System.Taffybar.Battery
 import System.Taffybar.MPRIS2
@@ -11,8 +13,10 @@ import System.Taffybar.SimpleClock
 import System.Taffybar.Systray
 import System.Taffybar.TaffyPager
 import System.Taffybar.Widgets.PollingGraph
+import System.Taffybar.Widgets.PollingLabel
 
 import System.Information.CPU
+import System.Information.DiskIO
 import System.Information.Memory
 
 cpuCallback :: IO [Double]
@@ -20,10 +24,10 @@ cpuCallback = do
     (_, _, totalLoad) <- cpuLoad
     return [totalLoad]
 
-memCallback :: IO [Double]
-memCallback = do
-    mi <- parseMeminfo
-    return [memoryUsedRatio mi]
+cpuCall :: IO String
+cpuCall = do
+    (_, _, totalLoad) <- cpuLoad
+    return $ show totalLoad
 
 green :: String
 green = "#B7F924"
@@ -31,6 +35,7 @@ red :: String
 red = "#FF2A24"
 blue :: String
 blue = "#12D5D5"
+yellow = "#f4bf75"
 
 hexTuplify :: String -> (Double, Double, Double, Double)
 hexTuplify = unlist . splitify
@@ -42,25 +47,39 @@ hexTuplify = unlist . splitify
     tuplify :: [String] -> [Double]
     tuplify = map ((/ 255) . fst . head . readHex)
 
+memToGB :: Int -> Double -> String
+memToGB dec size = printf ("%." ++ show dec ++ "f") $ size / 1024
+
+memLabel :: IO Gtk.Widget
+memLabel = do
+    label <- pollingLabelNew "" 2 $ do
+        let color pct
+              | pct < 0.5 = green
+              | pct < 0.9 = yellow
+              | otherwise = red
+        mi <- parseMeminfo
+        return $ "mem: <span fgcolor='" ++ color (memoryUsed mi / memoryTotal mi) ++ "'>"
+            ++ memToGB 2 (memoryUsed mi)
+            ++ "</span> / "
+            ++ memToGB 2 (memoryTotal mi)
+            ++ "GB"
+    Gtk.widgetShowAll label
+    return $ Gtk.toWidget label
+
 main :: IO ()
 main = do
-    let cpuCfg = defaultGraphConfig { graphDataColors = [hexTuplify green
-                                                        ,(0.02, 0.443, 0.565, 1)]
-                                    , graphLabel = Just "cpu"
-                                    }
-        memCfg = defaultGraphConfig { graphDataColors = [hexTuplify red]
-                                    , graphLabel = Just "mem"
-                                    }
-    let clock = textClockNew Nothing "<span fgcolor='#d58106'>%a %b %_d %H:%M:%S</span>" 1
+    let cpuCfg = defaultGraphConfig { graphDataColors = repeat (hexTuplify green)
+                                    , graphLabel = Just "cpu" }
+
+        clock = textClockNew Nothing "<span fgcolor='#d58106'>%a %b %_d %H:%M:%S</span>" 1
         cpu = pollingGraphNew cpuCfg 1 cpuCallback
-        mem = pollingGraphNew memCfg 1 memCallback
         inetformat = "<span fgcolor='" ++ green ++ "'>↓ $inKB$kB/s</span> <span fgcolor='" ++ red ++ "'>↑ $outKB$kB/s</span>"
         wlan = netMonitorNewWith 3 "wlp3s0" 1 inetformat
         wired = netMonitorNewWith 3 "eno1" 1 inetformat
         bat = batteryBarNew defaultBatteryConfig 30
         mpris = mpris2New
 
-        pager = taffyPagerNew defaultPagerConfig 
+        pager = taffyPagerNew defaultPagerConfig
             { activeWindow = colorize green "" . escape . shorten 40
             , activeWorkspace = colorize green "" . wrap " " " " . escape
             , activeLayout = colorize "#E921A8" "" . escape
@@ -70,6 +89,6 @@ main = do
             , widgetSep = colorize "#EF3600" "" " : "
             }
 
-    defaultTaffybar defaultTaffybarConfig { monitorNumber = 1
+    defaultTaffybar defaultTaffybarConfig { monitorNumber = 0
                                           , startWidgets = [ pager ]
-                                          , endWidgets = [ systrayNew, clock, mem, cpu, wlan, wired, bat, mpris ] }
+                                          , endWidgets = [ systrayNew, clock, memLabel, cpu, wlan, wired, bat, mpris ] }
--- .vim/ftplugin/haskell.vim
+++ .vim/ftplugin/haskell.vim
@@ -5,4 +5,6 @@ nmap <silent> <Space>hi :GhcModInfo<CR>
 
 let g:haskell_enable_pattern_synonyms = 1
 
+let g:haskellmode_completion_ghc = 0
+let g:ghcmod_use_basedir = getcwd()
 setlocal omnifunc=necoghc#omnifunc
